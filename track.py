ff8fb3cb12ab0c742b4e2397daeef0b70dbfe91c
André Reyes <areyesnav@gmail.com>
2014-08-13 09:24:59 -0500

    Update track.py
    se adiciono modulo para manifiestos import y export

--- track.py
+++ track.py
@@ -1,21 +1,39 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
+#!/usr/var/python
+#-*- coding:utf-8 -*-
 
 from bs4 import BeautifulSoup
 import requests
 import re
+import string
+import random
 soup = ''
-
 class Aduana():
-	def con_exp(self, manifiesto):
+		
+	def str_generator(self, size=6, chars=string.ascii_uppercase + string.digits):
+		return ''.join(random.choice(chars) for _ in range(size))
 
-		self = manifiesto
-
+	def mimp(self, manifiesto):
 		url = 'www.aduanet.gob.pe/servlet/CRManManif?CMc1_Anno=2014&CMc1_Numero=%s&CG_cadu=235&viat=4&CMc1_Terminal=0000&TipM=mc' % manifiesto
 		r = requests.get("http://" + url)
 		todo = r.text
 		soup = BeautifulSoup(todo)
 
-		f = open('aduana.txt','w')
+		f = open('aduana.xls','w')
 		f.write(soup.get_text().encode('utf-8'))
 		f.close()
+
+	def mexp(self, *manifiesto):
+		session = self.str_generator()
+		self = manifiesto
+		#www.aduanet.gob.pe/servlet/CRManManif;jsessionid=JLWsBc40J!18593?CMc1_Anno=2014&CMc1_Numero=1011&CG_cadu=235&viat=4&CMc1_Terminal=0000&TipM=mx'
+		for man in manifiesto:
+			url = 'www.aduanet.gob.pe/servlet/CRManManif;jsessionid=%s?CMc1_Anno=2014&CMc1_Numero=%s&CG_cadu=235&viat=4&CMc1_Terminal=0000&TipM=mx' % (session, manifiesto)
+			r = requests.get("http://" + url)
+			soup = BeautifulSoup(r.text)
+			calato = soup.get_text().encode('utf-8')	
+			
+			# magia = [s.strip() for s in calato.splitlines()]
+
+			f = open('aduana.txt','a')
+			f.write(str(calato))
+			f.close(
